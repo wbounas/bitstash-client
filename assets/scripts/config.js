961038df258916f4f1b11c168bc8124f5cf4be48
TedCart <TedCart@gmail.com>
2018-02-13 10:31:07 -0500

    Minor text change in development url config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    development: 'http://localHost:7165',
+    development: 'http://localhost:7165',
     production: 'https://desolate-eyrie-66077.herokuapp.com/'
   }
 }
