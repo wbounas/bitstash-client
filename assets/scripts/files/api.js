c198911dcbfdae923e870dc9d62da2a98953962d
TedCart <TedCart@gmail.com>
2018-02-14 17:18:35 -0500

    Changed GetFormFields to new FormData in events.js
    w/ @wbounas @nandodonaire @skinnybuff
    
    Also added a console log to the api.

--- assets/scripts/files/api.js
+++ assets/scripts/files/api.js
@@ -4,6 +4,7 @@ const config = require('../config')
 const store = require('../store')
 
 const createFile = function (data) {
+  console.log('data for ajax is: ', data)
   return $.ajax({
     url: 'http://localhost:4741/files',
     method: 'POST',
--- assets/scripts/files/events.js
+++ assets/scripts/files/events.js
@@ -8,11 +8,12 @@ const ui = require('./ui')
 const createUpload = function (event) {
   event.preventDefault()
   // console.log('this is the event.target', event.target)
-  // const data = new FormData(event.target)
-  const data = getFormFields(event.target)
+  const data = new FormData(event.target)
+  // const data = getFormFields(event.target)
   console.log(data)
-  // console.log('The data is ', data.get('image[file]'))
-  api.createFile(data)
+  console.log('The data is ', data.get('file[path]'))
+  api.createFile(data) // data is empty object :(((
+  // api.createFile(data.get('file[path]'))
     .then(ui.createFileSuccess)
     .catch(ui.createFileFailure)
 }
