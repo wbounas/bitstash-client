0eeab46d7914bc0f4e14f0c7c64e35da5256cbe9
skinnybuff <s_t_white@yahoo.com>
2018-02-14 10:26:53 -0500

    Add primitive forms & auth fields
    paired with @nandodonaire
    
    --added sign up, sign in, change password, sign out forms to HTML
    --added event js handlers to index.js
    --added events js with auth methods
    --added api js with ajax requests for auth
    --added ui js with user feed back for auth
    
    --TODO: update config js with api urls

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const store = require('../store')
+
+const signUpSuccess = function (data) {
+  // console.log(data)
+  $('#uiFeedback').text('Successfully signed up!')
+  $('#uiFeedback').css('color', 'green')
+}
+
+const signUpFailure = function (data) {
+  // console.error(error)
+  $('#uiFeedback').text('Sign up failed!')
+  $('#uiFeedback').css('color', 'red')
+}
+
+const signInSuccess = function (data) {
+  // console.log(data)
+  $('#uiFeedback').text('Successfully signed in!')
+  $('#uiFeedback').css('color', 'green')
+  // $('.logged-in').removeClass('hide')
+  // $('.game-functionality').removeClass('hide')
+  // $('.logged-out').addClass('hide')
+  store.user = data.user
+  // console.log('the stored data', store)
+}
+
+const signInFailure = function (data) {
+  // console.error(error)
+  $('#uiFeedback').text('Sign in failed!')
+  $('#uiFeedback').css('color', 'red')
+}
+
+const changePasswordSuccess = function (data) {
+  // console.log('Changed password!')
+  $('#uiFeedback').text('Changed password!')
+  $('#uiFeedback').css('color', 'green')
+}
+
+const changePasswordFailure = function (data) {
+  // console.error(error)
+  $('#uiFeedback').text('Error changing password!')
+  $('#uiFeedback').css('color', 'red')
+}
+
+const signOutSuccess = function () {
+  // console.log('Signed out!')
+  $('#uiFeedback').text('Signed out!')
+  $('#uiFeedback').css('color', 'green')
+  // $('.logged-in').addClass('hide')
+  // $('.game-functionality').addClass('hide')
+  // $('.game-board').addClass('hide')
+  // $('.logged-out').removeClass('hide')
+  // $('.game-search').addClass('hide')
+  // $('.games-search').addClass('hide')
+  store.user = null
+}
+
+const signOutFailure = function () {
+  $('#uiFeedback').text('Failed signing out!')
+  $('#uiFeedback').css('color', 'red')
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
